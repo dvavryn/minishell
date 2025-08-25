6c7bbb83d10463a9958cecdb6fe849fea5bbc8b7
bschwarz <bschwarz@student.42vienna.com>
2025-08-25 15:44:20 +0200

    changed function positions

--- src/free_tokens.c
+++ src/free_tokens.c
@@ -6,11 +6,25 @@
 /*   By: bschwarz <bschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 13:10:24 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/08/25 15:29:12 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/08/25 15:43:17 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	free_tokens(t_token *token)
+{
+	t_token	*tmp;
+
+	while (token)
+	{
+		tmp = token->next;
+		if (token->value)
+			free(token->value);
+		free(token);
+		token = tmp;
+	}
+}
 
 void	print_tokens(t_token *token)
 {
--- src/parsing_ops.c
+++ src/parsing_ops.c
@@ -6,25 +6,13 @@
 /*   By: bschwarz <bschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 12:47:58 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/08/25 15:28:56 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/08/25 15:43:10 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	free_tokens(t_token *token)
-{
-	t_token	*tmp;
 
-	while (token)
-	{
-		tmp = token->next;
-		if (token->value)
-			free(token->value);
-		free(token);
-		token = tmp;
-	}
-}
 
 char	*read_quotes(char *input, ssize_t *i)
 {
