046537644caceafe90a606ab8a5f14b831aeb417
Dominic Vavryn <dvavryn@c3r2p5.42vienna.com>
2025-08-27 15:54:49 +0200

    semi working environment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 15:05:32 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/08/27 15:09:18 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/08/27 15:54:15 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,57 @@ char **copy_env(char **envp)
 	return (out);
 }
 
+int update_env(t_data *data)
+{
+	ssize_t	i;
+	char *tmp1;
+	char *tmp2;
+	
+	i = -1;
+	while (data->env[++i])
+	{
+		if (!ft_strncmp("SHELL=", data->env[i], 6))
+		{
+			tmp1 = ft_strdup("SHELL=/minishell??");	// TODO: finish path search
+			if (!tmp1)
+				return (1);
+			free(data->env[i]);
+			data->env[i] = tmp1;
+		}
+		else if (!ft_strncmp("SHLVL", data->env[i], 6))
+		{
+			tmp2 = ft_itoa(ft_atoi(&data->env[i][6]) + 2); // TODO: doesnt work properly
+			if (!tmp2)
+				return (1);
+			tmp1 = ft_strjoin("SHLVL", tmp2);
+			free(tmp2);
+			free(data->env[i]);
+			data->env[i] = tmp1;
+		}
+	}
+	return (0);
+}
+
+void	free_data(t_data *data)
+{
+	if (data->env)
+		free_split(data->env);
+	if (data->input)
+		free(data->input);
+	if (data->token)
+		free_tokens(data->token);
+}
 int	get_data(t_data *data, char **envp)
 {
 	data->env = copy_env(envp);
 	if (!data->env)
 		return (1);
+	// SHELL SHLVL
+	if (update_env(data))
+	{
+		free_data(data);
+		return (2);
+	}
 	return (0);
 }
 
@@ -80,7 +126,7 @@ int	main(int argc, char **argv, char **envp)
 			data.token = lex_input(data.input);
 			if (data.token)
 			{
-				if (ft_strcmp("exe", data.token->value))
+				if (!ft_strcmp("exe", data.token->value))
 				{
 					printf("going into execution mode!\n");
 					ft_executor(data.token->next, data.env);
