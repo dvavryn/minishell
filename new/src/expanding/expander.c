f85ed53645bbc87d6dca2fcf570ae95238915b3a
Dominic Vavryn <dvavryn@c3r1p6.42vienna.com>
2025-09-04 16:11:26 +0200

    chagnes

--- new/src/expanding/expander.c
+++ new/src/expanding/expander.c
@@ -6,24 +6,54 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 14:15:50 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/09/04 14:41:11 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/09/04 16:05:39 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// static char *join(char *in, char *value, size_t len)
+// {
+// 	char *out;
+// 	char *tmp;
+
+// 	tmp = ft_substr(value, 0, len);
+// 	if (!tmp)
+// 	{
+// 		free(in);
+// 		return (NULL);
+// 	}
+// 	if (!in)
+// 		out = ft_strjoin("", tmp);
+// 	else
+// 		out = ft_strjoin("")
+	
+// }
+
 static void	expand_word(t_data *data, char *value)
 {
-	t_list	*list;
 	ssize_t	i;
+	ssize_t	j;
+	char *tmp;
+	char *buf;
+	
+	(void)data;
 
-	
 	i = -1;
-	while (value[++i]);
+	buf = NULL;
+	while (value[++i])
 	{
+		j = -1;
 		if (value[i] == '$')
+			j++;
+		while (value[++j + i] && value[j + i] != '$')
+			;
+		buf = ft_substr(&value[i], 0, j);
+		if (!buf)
+			ft_exit(data, MAL_ERR, MAL_ERR);
+		printf("%s\n", buf);
+		i += j - 1;
 	}
-	
 }
 
 void	expander(t_data *data)
@@ -34,7 +64,10 @@ void	expander(t_data *data)
 	while (ptr)
 	{
 		if (ptr->value && ft_strchr(ptr->value, '$') && ptr->type == TOKEN_WORD && ptr->quote != QUOTE_SINGLE)
-			printf("%s\n", ptr->value);
+		{
+			expand_word(data, ptr->value);
+			// printf("%s\n", ptr->value);
+		}
 		ptr = ptr->next;
 	}
 }
