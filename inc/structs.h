0eb6e7df9a4623d01e1f55d9d93f89095bdd5f1a
Dominic Vavryn <dvavryn@c3r7p11.42vienna.com>
2025-09-12 13:55:57 +0200

    changes

--- inc/structs.h
+++ inc/structs.h
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 10:45:56 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/09/01 14:46:19 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/09/12 13:55:50 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,28 @@ typedef struct s_token
 	struct s_token	*next;
 }	t_token;
 
+typedef struct s_cmd
+{
+	char			*cmd;		// first TOKEN_WORD
+	char			**args;		// every other TOKEN_WORD before the next special character, | < > << >>
+	int				pipe_in;	// every command besides the first one
+	int				pipe_out;	// if there is a next command;
+	char			*file_in;	// infile filename, if if R_HEREDOC limiter
+	int				redir_in;	// infile flag if R_IN or R_HEREDOC
+	char			*file_out;	// otufile filename 
+	int				redir_out;	// outfile flag if R_OUT or R_APPEND
+	int				fd_in;		// init at -1;
+	int				fd_out;		// init at -1;
+	struct s_cmd	*next;		// ptr to next command
+}	t_cmd;
+
 typedef struct s_data
 {
 	char	**env;
-	t_token	*token;
 	char	*input;
+	t_token	*token;
+	t_cmd	*cmd;
+	size_t	line;
 	ssize_t	i;
 }	t_data;
 
