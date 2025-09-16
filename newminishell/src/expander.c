c17dd62273a3acc4bc0de77bf7677311170a04be
Dominic Vavryn <dvavryn@c3r7p11.42vienna.com>
2025-09-16 13:58:20 +0200

    changes

--- newminishell/src/expander.c
+++ newminishell/src/expander.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 20:16:39 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/09/15 16:59:11 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/09/16 12:48:07 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,47 @@
 // TODOTODOTODOTODOTODOTODO
 // TODOTODOTODOTODOTODOTODO
 
-void	expand_str(t_data *data, char **s)
+void	jump_quotes(char *s, int c, size_t *i)
 {
-	(void)data;
-	(void)s;
+	while (ptr[i] && ptr[i])
+}
+
+char	*expand_str(t_data *data, char *s)
+{
+	size_t	i;
+	char	*out = NULL;
+	char	*buf[2];
+	char	*ptr;
+
+	i = 0;
+	ptr = s;
+	out = ft_strdup("");
+	while (s[i])
+	{
+		i = 0;
+		while (ptr[i] && ptr[i] != '$')
+		{
+			if (ptr[i] == '\'')
+				jump_quotes(ptr, ptr[i], &i);
+			i++;
+		}
+		buf[0] = ft_substr(ptr, 0, i);
+		if (!buf[0])
+		{
+			free(out);
+			ft_exit(data, "malloc");
+		}
+		buf[1] = ft_strjoin(out, buf[0]);
+		free(buf[0]);
+		if (!buf[1])
+			ft_exit(data, "maloc");
+		free(out);
+		out = buf[1];
+		ptr += i;
+	}
+	if (!out)
+		ft_exit(data, "malloc");
+	return (out);
 }
 
 void	expand(t_data *data, t_cmd *cmd)
@@ -40,16 +77,16 @@ void	expand(t_data *data, t_cmd *cmd)
 	ssize_t	i;
 
 	if (!ft_strchr(cmd->cmd, '\''))
-		expand_str(data, &cmd->cmd);
+		cmd->cmd = expand_str(data, cmd->cmd);
 	if (cmd->redir_in == R_IN && !ft_strchr(cmd->file_in, '\''))
-		expand_str(data, &cmd->file_in);
+		cmd->file_in = expand_str(data, cmd->file_in);
 	if (cmd->redir_out)
-		expand_str(data, &cmd->file_out);
+		cmd->file_out = expand_str(data, cmd->file_out);
 	i = -1;
 	while (cmd->args[++i])
 	{
 		if (!ft_strchr(cmd->args[i], '\''))
-			expand_str(data, &cmd->args[i]);
+			cmd->args[i] = expand_str(data, cmd->args[i]);
 	}
 }
 
