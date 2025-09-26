7185d57e64fff91c0832a23c9bab4e071e833156
Dominic Vavryn <dvavryn@c3r1p4.42vienna.com>
2025-09-26 17:38:20 +0200

    changes

--- src/free/free_cmd.c
+++ src/free/free_cmd.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 17:15:14 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/09/25 17:15:19 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/09/26 17:38:02 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	free_cmd(t_cmd *cmd)
 		ptr = cmd->next;
 		if (cmd->cmd)
 			free(cmd->cmd);
-		if (cmd->file_in && !ft_strncmp(cmd->file_in, ".heredoc", 8))
+		if (cmd->file_in && cmd->redir_in == R_HEREDOC)
 			unlink(cmd->file_in);
 		if (cmd->file_in)
 			free(cmd->file_in);
