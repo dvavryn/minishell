e9f7f4baf89f69a9f83994b5520bddb616deeffe
bschwarz <bschwarz@student.42vienna.com>
2025-10-15 21:12:31 +0200

    cd

--- src/builtin/bi_cd.c
+++ src/builtin/bi_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bi_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: bschwarz <bschwarz@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 16:11:15 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/10/15 20:35:35 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/10/15 21:12:01 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,18 @@ static int assign_pwd(t_data *data, char *pwd, ssize_t i)
 
 int	bi_cd(t_data *data, t_cmd *cmd)
 {	
+	char	*old;
+	
+	if (!ms_getenv(data->env, "OLDPWD"))
+		old = NULL;
+	else
+		old = ft_strdup(ms_getenv(data->env, "OLDPWD"));
 	if(assign_pwd(data, "OLDPWD=", 0))
 		return (1);
 	if (!cmd->args[1])
 		chdir(ms_getenv(data->env, "HOME"));
+	else if (cmd->args[1][0] == '-')
+		chdir(old);
 	else if ((chdir(cmd->args[1])) == -1)
 		return (perror("minishell: cd"), 1);
 	if (assign_pwd(data, "PWD=", 0))
