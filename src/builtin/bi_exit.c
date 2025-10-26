cecbcbaa8484c4780210ddf9420ee467a9f8a5fb
Dominic Vavryn <vavryn.dominic@outlook.com>
2025-10-26 15:28:50 +0100

    added not exiting with nonnumerical argument

--- src/builtin/bi_exit.c
+++ src/builtin/bi_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bi_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: dvavryn <dvavryn@sudent.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 15:22:44 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/09/25 15:22:45 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/10/26 15:11:10 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,17 @@ void	bi_exit(t_data	*data, char **args)
 	if (!args || !args[1])
 		ret = 0;
 	else
+	{
 		ret = ft_atoi(args[1]);
+		if (ret == 0 && args[1][0] != 0)
+		{
+			ft_putstr_fd("minishell: ", STDOUT_FILENO);
+			ft_putstr_fd(args[1], STDOUT_FILENO);
+			ft_putendl_fd(": numeric argument required", STDOUT_FILENO);
+			data->ret = 2;
+			return ;
+		}
+	}
 	free_all(data);
 	exit(ret);
 }
