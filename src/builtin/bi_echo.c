8d6d8d403996991420d07544be293fa48818a179
Dominic Vavryn <vavryn.dominic@outlook.com>
2025-10-26 15:27:50 +0100

    fixed 'echo' to print an empty line

--- src/builtin/bi_echo.c
+++ src/builtin/bi_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bi_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: dvavryn <dvavryn@sudent.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 14:57:30 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/10/15 19:53:28 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/10/26 15:05:45 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	bi_echo(t_data *data, char **args)
 			if (write(STDOUT_FILENO, " ", 1) == -1)
 				return (-1);
 	}
-	if (args[1] && ft_strcmp(args[1], "-n"))
+	if (!args[1] || (args[1] && ft_strcmp(args[1], "-n")))
 		if ((write(STDOUT_FILENO, "\n", 1)) == -1)
 			return (-1);
 	return (0);
